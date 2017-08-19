7ab8b95aca98a7ae4d2ccaf2692c61181a44f32f
viinay <viinay@users.noreply.github.com>
2017-08-19 22:53:17 +0530

    Create refactor_type.ts

---
+++ refactor_type.ts
@@ -0,0 +1,16 @@
+// src/app/hero.ts
+export class Hero {
+  id:number;
+  name:string;
+ }
+ 
+// src/app/hero.component.ts
+import { Hero } from './hero';
+...
+superhero:Hero = {
+  id:1,
+  name:'superman'
+ }
+ ...
+ 
+ 
